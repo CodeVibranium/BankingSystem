baa882eb64877c3f5daa1b3307f7d2394a6076d2
Shahed_ali <mohammedshahedali464@gmail.com>
2021-03-30 12:09:48 +0530

    Layout created

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+Layout how system should work.
+select an option
+1.Open an account : FName , Lname ,Id, Key;
+2.Balance Enquiry :Id ,Key,Shows balance;
+3.Log history: Widthdrawls,Deposits,account number,Id;
+4.Deposit:Account number , money;
+5.Withdraw: Acc number ,Id, Key,amount required;
+6.Close account:Account number, Id, Key;
+7.Exit;
+/*8. SuperUser:Show all accounts.:Shows all account holder names and numbers.
+*/
