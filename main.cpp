ddbe904e45fb60e8c500af36409a14d9bdfdf721
Shahed_ali <mohammedshahedali464@gmail.com>
2021-03-30 16:22:00 +0530

    2.All necessary inputs added.

--- main.cpp
+++ main.cpp
@@ -12,16 +12,31 @@ select an option
 */
 
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 class BankingSystem
 {
- public:
+private:
+ string FirstName;
+ string LastName;
+ int Id;
+ int Key;
+ float Deposit;
+ float Withdrawl;
+public:
  int accountNumber=0;
+ int id;
+ int Balance;
+
  //Parametrized Constructors
- BankingSystem()
+ BankingSystem(string F="none",string L="none",int key=0,float deposit=0.0,float withdrawl=0.0 )
  {
-      
+    setName(F, L);
+    setId();
+    setKey(key);
+    setDeposit(deposit);
+    setWithdrawl(withdrawl);
  }
  //Mutators
  void setAccountNumber()
@@ -29,11 +44,69 @@ class BankingSystem
      accountNumber=accountNumber+1;
      cout<<"Account number : "<<accountNumber<<endl;
  }
- 
+ void setName(string F,string L)
+ {
+    FirstName=F;
+    LastName=L;
+ }
+
+ void setId()
+ {  id = rand() % 80 + 19855149; 
+    Id=id;
+ }
+ void setKey(int key)
+ {
+     Key=key;
+ }
+ void setDeposit(float deposit)
+ {
+     Deposit=deposit;
+ }
+ void setWithdrawl(float withdrawl)
+ {
+     if(withdrawl>Deposit)
+         cout<<"You dont enough funds!!";
+    else
+     Withdrawl=withdrawl;
+ }
+ float showBalance()
+ {
+     return -Withdrawl+Deposit;
+ }
+
+//  Accessors
+ int getId() 
+ {
+     return Id;
+ }
 };
 
 int main()
 {
+    int key;
+    string F,L;
+    float deposit,withdrawl;
+
     BankingSystem a;
+
     a.setAccountNumber();
+    cout<<"Enter Your First Name : ";
+    getline(cin,F);
+    cout<<"Enter your Last Name : ";
+    getline(cin,L);
+    a.setName(F,L);
+    a.setId();
+    cout<<"This is your account id : "<<a.getId()<<endl;
+    cout<<"Enter your password : ";
+    cin>>key;
+    a.setKey(key);
+    cout<<"Enter the amount you want to deposit : ";
+    cin>>deposit;
+    a.setDeposit(deposit);
+    cout<<"Enter the amount you want to withdraw : ";
+    cin>>withdrawl;
+    a.setWithdrawl(withdrawl);
+    cout<<"Your balance : "<<a.showBalance();
+
+    return 0;
 }
